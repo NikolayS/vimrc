f1ab9a7091bfc52658ef97c4fd885decd17c3f20
NikolayS <github2@samokhvalov.com>
2016-09-28 05:49:25 +0300

    Update and rename ubuntu_curl_awesome.sh to quickinstall_awesome.sh

---
+++ quickinstall_awesome.sh
@@ -0,0 +1,5 @@
+apt-get -v &> /dev/null && sudo add-apt-repository ppa:jonathonf/vim && sudo apt update && sudo apt install -y vim
+#which yum &> /dev/null && yum update
+#which brew &> /dev/null && brew install --HEAD vim
+git clone https://github.com/NikolayS/vimrc.git ~/.vim_runtime
+sh ~/.vim_runtime/install_awesome_vimrc.sh
--- ubuntu_curl_awesome.sh
+++
@@ -1,3 +0,0 @@
-sudo add-apt-repository ppa:jonathonf/vim && sudo apt update && sudo apt install -y vim
-git clone https://github.com/NikolayS/vimrc.git ~/.vim_runtime
-sh ~/.vim_runtime/install_awesome_vimrc.sh
